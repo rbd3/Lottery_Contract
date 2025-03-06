17abbd320b127d0bacbef362e88e54561731be79
rbd3 <rabedesanaandry01@gmail.com>
2025-03-06 18:06:33 +0300

    Add constructor

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
-contract Raffle {}
+/**
+ * @title Raffle contract
+ * @author Andry Narson
+ * @notice This contract is for creating a sample raffle
+ * @dev implements chainlink
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+}
